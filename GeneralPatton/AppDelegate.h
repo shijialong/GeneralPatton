482481b9c7b8c1a5412814e772ea6ef89014613b
shijialong <shijialong8@sina.com>
2017-12-13 11:56:20 +0800

    Initial Commit

---
+++ GeneralPatton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeneralPatton
+//
+//  Created by shijialong on 2017/12/13.
+//  Copyright © 2017年 shijialong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeneralPatton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeneralPatton
+//
+//  Created by shijialong on 2017/12/13.
+//  Copyright © 2017年 shijialong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
